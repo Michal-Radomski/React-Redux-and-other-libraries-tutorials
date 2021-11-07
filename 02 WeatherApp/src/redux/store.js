f159d9bba27562fefa7ae35a16bb3d5255446138
Michal Radomski <michalra@op.pl>
2021-11-07 18:35:40 +0100

    Weahter App done

--- 02 WeatherApp/src/redux/store.js
+++ 02 WeatherApp/src/redux/store.js
@@ -1,7 +1,9 @@
 import {configureStore} from "@reduxjs/toolkit";
 
+import weatherSlice from "./weatherSlices";
+
 const store = configureStore({
-  reducer: {},
+  reducer: weatherSlice,
 });
 
 export default store;
--- 02 WeatherApp/src/redux/weatherSlices.js
+++ 02 WeatherApp/src/redux/weatherSlices.js
@@ -2,14 +2,15 @@ import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_OPEN_WEATHER_KEY;
-console.log("API_KEY:", API_KEY);
+// console.log("API_KEY:", API_KEY);
 
 // Action
 export const fetchWeatherAction = createAsyncThunk(
   "weather/fetch",
   async (payload, {rejectWithValue, getState, dispatch}) => {
     try {
-      const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${API_KEY}`);
+      const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${payload}&appid=${API_KEY}`);
+      const data = response?.data;
       return data;
     } catch (error) {
       if (!error?.response) {
@@ -19,3 +20,34 @@ export const fetchWeatherAction = createAsyncThunk(
     }
   }
 );
+
+// Slices
+const weatherSlice = createSlice({
+  name: "weather",
+  initialState: {
+    loading: false,
+    error: undefined,
+  },
+  extraReducers: (builder) => {
+    //Pending
+    builder.addCase(fetchWeatherAction.pending, (state, action) => {
+      state.loading = true;
+      state.weather = undefined;
+      state.error = undefined;
+    });
+    //Fulfilled
+    builder.addCase(fetchWeatherAction.fulfilled, (state, action) => {
+      state.weather = action?.payload;
+      state.loading = false;
+      state.error = false;
+    });
+    //Rejected
+    builder.addCase(fetchWeatherAction.rejected, (state, action) => {
+      state.loading = false;
+      state.weather = undefined;
+      state.error = action?.payload;
+    });
+  },
+});
+
+export default weatherSlice.reducer;
