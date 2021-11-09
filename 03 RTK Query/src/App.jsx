052ceca750873af8a485368af3658164aceaaf35
Michal Radomski <michalra@op.pl>
2021-11-09 15:35:53 +0100

    RTK query app done

--- 03 RTK Query/src/App.jsx
+++ 03 RTK Query/src/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+
 import "./App.scss";
+import {useGetUsersQuery} from "./users";
 
 function App() {
-  return <React.Fragment>App</React.Fragment>;
+  // console.log("useGetUsersQuery:", useGetUsersQuery);
+  const {data, error, isLoading, isSuccess, isError} = useGetUsersQuery();
+  // console.log("data, error, isLoading, isSuccess, isError:", data, error, isLoading, isSuccess, isError);
+  return (
+    <div style={{textAlign: "center"}}>
+      <h1>React Redux Toolkit RTK Query</h1>
+      {/* //-  BELOW: if isLoading true => "Loading..." */}
+      {isLoading && "Loading..."}
+      {isError && error.message}
+      {isSuccess && data && data.map((user) => <h4 key={user.id}>{user.name}</h4>)}
+    </div>
+  );
 }
 
 export default App;
