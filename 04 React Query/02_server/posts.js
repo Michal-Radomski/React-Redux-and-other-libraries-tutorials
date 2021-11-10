733108953a5595e15a4aaf8567d017792892ed7f
Michal Radomski <michalra@op.pl>
2021-11-10 19:46:46 +0100

    migration posts.js to posts.ts

--- 04 React Query/02_server/posts.js
+++ 04 React Query/02_server/posts.js
@@ -1,21 +1,11 @@
 var faker = require("faker");
-
-interface Post {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  details: string;
-}
-
 function generatePosts() {
-  var posts: Post[] = [];
+  var posts = [];
   for (var id = 1; id <= 25; id++) {
     var firstName = faker.name.firstName();
     var lastName = faker.name.lastName();
     var email = faker.internet.email();
     var details = faker.lorem.words();
-
     posts.push({
       id: id,
       first_name: firstName,
