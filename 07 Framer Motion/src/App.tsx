5323f74460f47069d61e02ad364cd27ce4f269c1
Michal Radomski <michalra@op.pl>
2022-02-07 22:23:48 +0100

    Exercise1 done

--- 07 Framer Motion/src/App.tsx
+++ 07 Framer Motion/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
-import "./App.scss";
 
-function App() {
-  return <React.Fragment>App</React.Fragment>;
+import "./App.scss";
+import Box1 from "./components/Box1/Box1";
+
+function App(): JSX.Element {
+  return (
+    <React.Fragment>
+      <Box1 />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ 07 Framer Motion/src/components/Box1/Box1.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {motion} from "framer-motion";
+
+function Box1(): JSX.Element {
+  const [isAnimated, setIsAnimated] = React.useState<boolean>(false);
+
+  return (
+    <div className="box-container">
+      <motion.div
+        className="box"
+        initial={{opacity: 0.2}}
+        animate={{
+          x: isAnimated ? "1000px" : 0,
+          opacity: isAnimated ? 1 : 0.2,
+          // backgroundColor: "blue",
+          // scale: 1.5,
+          rotate: isAnimated ? 360 : 0,
+        }}
+        transition={{
+          stiffness: 400,
+          type: "spring",
+          damping: 10,
+        }}
+        onClick={() => {
+          setIsAnimated(!isAnimated);
+        }}
+      ></motion.div>
+    </div>
+  );
+}
+
+export default Box1;
