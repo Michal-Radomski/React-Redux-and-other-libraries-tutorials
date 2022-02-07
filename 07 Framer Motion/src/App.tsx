c8146c55a227798888ec2f953e794e644e25abda
Michal Radomski <michalra@op.pl>
2022-02-07 22:35:23 +0100

    Exercise2 done

--- 07 Framer Motion/src/App.tsx
+++ 07 Framer Motion/src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./App.scss";
 import Box1 from "./components/Box1/Box1";
+import Box2 from "./components/Box2/Box2";
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
       <Box1 />
+      <Box2 />
     </React.Fragment>
   );
 }
---
+++ 07 Framer Motion/src/components/Box2/Box2.tsx
@@ -0,0 +1,23 @@
+import {motion} from "framer-motion";
+
+function Box2(): JSX.Element {
+  return (
+    <div className="box-container">
+      <motion.div
+        className="box"
+        whileHover={{scale: 1.1}}
+        whileTap={{scale: 0.8}}
+        drag={true}
+        dragConstraints={{
+          right: 20,
+          left: -20,
+          top: 5,
+          bottom: 5,
+        }}
+        //  drag="y"
+      ></motion.div>
+    </div>
+  );
+}
+
+export default Box2;
