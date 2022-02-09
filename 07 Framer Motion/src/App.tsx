f297fa76bbb03abc79f8e79fbed18159b0a7e463
Michal Radomski <michalra@op.pl>
2022-02-09 03:25:46 +0100

    Exercise5 done

--- 07 Framer Motion/src/App.tsx
+++ 07 Framer Motion/src/App.tsx
@@ -5,6 +5,7 @@ import Box1 from "./components/Box1";
 import Box2 from "./components/Box2";
 import Box3 from "./components/Box3";
 import Box4 from "./components/Box4";
+import Box5 from "./components/Box5";
 
 function App(): JSX.Element {
   return (
@@ -13,6 +14,7 @@ function App(): JSX.Element {
       <Box2 />
       <Box3 />
       <Box4 />
+      <Box5 />
     </React.Fragment>
   );
 }
