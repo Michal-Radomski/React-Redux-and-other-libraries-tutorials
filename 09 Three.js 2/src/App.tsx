f922f184ae497492caadfecd5fb43b6906a8d7c2
Michal Radomski <michalra@op.pl>
2022-02-11 22:18:17 +0100

    Mars_3D.glb added

--- 09 Three.js 2/src/App.tsx
+++ 09 Three.js 2/src/App.tsx
@@ -1,8 +1,29 @@
 import React from "react";
+
+import styled from "styled-components";
+import {Canvas} from "@react-three/fiber";
+
 import "./App.scss";
 
+import MarsSphere from "./components/MarsSphere";
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
 function App() {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <>
+      <CanvasContainer>
+        <Canvas>
+          <React.Suspense fallback={null}>
+            <MarsSphere />
+          </React.Suspense>
+        </Canvas>
+      </CanvasContainer>
+    </>
+  );
 }
 
 export default App;
