a8f719203cf6f099c25f3fab9abfcbe874dd4a98
Michal Radomski <michalra@op.pl>
2022-02-13 23:42:41 +0100

    Logo was added

--- 09 Three.js 2/src/components/StarWarsCrawl/StarWarsCrawl.tsx
+++ 09 Three.js 2/src/components/StarWarsCrawl/StarWarsCrawl.tsx
@@ -1,13 +1,38 @@
 import React from "react";
+import {gsap} from "gsap";
 
 import "./fonts/FranklinGothicBook.ttf";
+import Logo from "./media/logo.png";
+
 import "./StarWarsCrawl.scss";
 
-const StarWarsCrawl = () => {
+const StarWarsCrawl = (): JSX.Element => {
+  const intro = React.useRef<HTMLDivElement>(null);
+  const title = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    let tl = gsap.timeline();
+    //*Below the same
+    // gsap.to(intro.current, {opacity: 1, delay: 1, duration: 4.5});
+    // gsap.to(intro.current, {opacity: 0, delay: 5.5, duration: 1.5});
+    tl.to(intro.current, {opacity: 1, delay: 1, duration: 4.5}).to(intro.current, {opacity: 0, duration: 1.5});
+  }, []);
+
   return (
-    <React.Fragment>
-      <div className="StarWarsMain">StarWarsCrawl</div>;
-    </React.Fragment>
+    <>
+      <div className="StarWarsMain">
+        <section className="intro" ref={intro}>
+          <p>
+            Some time ago, in the Solar System on Mars,
+            <br /> quite not that far ...
+          </p>
+        </section>
+        <section className="title" ref={title}>
+          <img src={Logo} alt="Code Wars title" />
+        </section>
+      </div>
+      ;
+    </>
   );
 };
 
