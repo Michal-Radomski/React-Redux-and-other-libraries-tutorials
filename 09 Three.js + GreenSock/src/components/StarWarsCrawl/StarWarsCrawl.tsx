9cf8f470bb475c5840f8393cb3bce927e81eb31f
Michal Radomski <michalra@op.pl>
2022-02-14 02:48:31 +0100

    music was added

--- 09 Three.js + GreenSock/src/components/StarWarsCrawl/StarWarsCrawl.tsx
+++ 09 Three.js + GreenSock/src/components/StarWarsCrawl/StarWarsCrawl.tsx
@@ -3,6 +3,8 @@ import {gsap} from "gsap";
 
 import "./fonts/FranklinGothicBook.ttf";
 import Logo from "./media/logo.png";
+import Mute from "./media/mute.png";
+import Volume from "./media/volume.png";
 
 import "./StarWarsCrawl.scss";
 
@@ -10,6 +12,9 @@ const StarWarsCrawl = (): JSX.Element => {
   const intro = React.useRef<HTMLDivElement>(null);
   const title = React.useRef<HTMLDivElement>(null);
   const content = React.useRef<HTMLDivElement>(null);
+  const audio = React.useRef<any>();
+
+  const [muted, setMuted] = React.useState<boolean>(true);
 
   React.useEffect(() => {
     let tl = gsap.timeline();
@@ -17,8 +22,14 @@ const StarWarsCrawl = (): JSX.Element => {
     // gsap.to(intro.current, {opacity: 1, delay: 1, duration: 4.5});
     // gsap.to(intro.current, {opacity: 0, delay: 5.5, duration: 1.5});
     tl.to(intro.current, {opacity: 1, delay: 1, duration: 4.5})
-      .to(intro.current, {opacity: 0, duration: 1.5})
-      .set(title.current, {opacity: 1, scale: 2.75})
+      .to(intro.current, {
+        opacity: 0,
+        duration: 1.5,
+        onComplete: () => {
+          audio.current.play();
+        },
+      })
+      .set(title.current, {opacity: 1, scale: 2.75, delay: 0.5})
       .to(title.current, {scale: 0.05, ease: "power3.inOut", duration: 8})
       .to(title.current, {opacity: 0, duration: 1.5}, "-=1.5")
       .to(content.current, {top: "-170%", duration: 20})
@@ -55,8 +66,21 @@ const StarWarsCrawl = (): JSX.Element => {
             </p>
           </div>
         </section>
+
+        <audio ref={audio} muted>
+          <source type="audio/mpeg" src="https://ia801501.us.archive.org/23/items/StarWars_20180709/Star%20Wars.mp3" />
+        </audio>
+        <button
+          className="volume"
+          type="button"
+          onClick={() => {
+            audio.current.muted = !muted;
+            setMuted(!muted);
+          }}
+        >
+          {muted ? <img src={Mute} alt="Volume is off" /> : <img src={Volume} alt="Volume is on" />}
+        </button>
       </div>
-      ;
     </>
   );
 };
